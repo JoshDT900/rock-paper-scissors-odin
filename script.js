bef3a86f89080cfd563c10670402637d52f84770
JoshDT900 <DJTroutman900@gmail.com>
2022-06-05 18:08:04 -0400

    Complete rock paper scissors script

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 let userChoice = window.prompt("Choose either Rock, Paper, or Scissors.")
+let cpuChoice = ["rock", "paper", "scissors"]
+
+cpuChoice = cpuChoice[Math.floor(Math.random() * 3)]
+userChoice = userChoice.toLowerCase();
+
+
+function rockPaperScissors(userChoice, cpuChoice) {
+
+
+  if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+    return alert("Invalid choice, please pick rock, paper, or scissors");
+  }
+
+  if (userChoice === cpuChoice) {
+    return "It's a tie!"
+  } else if (userChoice === "rock" && cpuChoice === "scissors" || userChoice === "paper" && cpuChoice === "rock") {
+    return "You win!"
+  } else if (userChoice === "rock" && cpuChoice === "paper" || userChoice === "paper" && cpuChoice === "scissors") {
+    return "You lose!";
+  }
+}
+
+console.log(rockPaperScissors(userChoice, cpuChoice));

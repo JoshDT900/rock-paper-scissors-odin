64e15600bc8fd792724023407ba092bf8c055f90
JoshDT900 <DJTroutman900@gmail.com>
2022-06-05 19:40:12 -0400

    Finish RPS project

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let userChoice = window.prompt("Choose either Rock, Paper, or Scissors.")
-let cpuChoice = ["rock", "paper", "scissors"]
-
-cpuChoice = cpuChoice[Math.floor(Math.random() * 3)]
-userChoice = userChoice.toLowerCase();
-
 let playerOutput = document.querySelector(".userChoice")
 let cpuOutput = document.querySelector(".computerChoice")
 let resultOutput = document.querySelector(".winnerMessage")
+let playerScore = document.querySelector(".playerScore")
+let cpuScore = document.querySelector(".cpuScore")
+let tiesScore = document.querySelector(".ties")
 
-function computerPlay(userChoice, cpuChoice) {
+function computerPlay() {
+  cpuChoice = ["rock", "paper", "scissors"]
 
+  return cpuChoice[Math.floor(Math.random() * 3)];
+}
+
+function round(userChoice, cpuChoice) {  
+  userChoice = userChoice.toLowerCase();
+  
   if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
     return resultOutput.textContent = "Invalid choice, please pick rock, paper, or scissors.";
   }
@@ -23,6 +27,45 @@ function computerPlay(userChoice, cpuChoice) {
   }
 }
 
-playerOutput.textContent = `The player chose ${userChoice}`
-cpuOutput.textContent = `The compyter chose ${cpuChoice}`
-resultOutput.textContent = `The results is: ${computerPlay(userChoice, cpuChoice)}`
+function game() {
+  let cpuWins = 0;
+  let playerWins = 0;
+  let tiesNum = 0;
+
+  for (let i = 0; i < 5; i++) {
+    cpuChoice = computerPlay();
+    userChoice = window.prompt("Choose either Rock, Paper, or Scissors.") 
+    if (round(userChoice, cpuChoice) === "It's a tie!") {
+      tiesNum++      
+      resultOutput.textContent = "No one gets a point";
+      tiesScore.textContent = `Number of ties: ${tiesNum}`
+    } else if (round(userChoice, cpuChoice) === "You win!") {
+      playerWins++     
+      playerScore.textContent = `Player Score: ${playerWins}`;
+      cpuScore.textContent = `CPU Score: ${cpuWins}`
+    } else if (round(userChoice, cpuChoice) === "You lose!") {    
+      cpuWins++;
+      playerScore.textContent = `Player Score: ${playerWins}`
+      cpuScore.textContent = `CPU Score: ${cpuWins}`;
+    } else {
+      alert("Please choose either rock, paper, or scissors")
+      i--;
+    }
+  }
+
+  if (cpuWins < playerWins) {
+    return resultOutput.textContent = `The results is: You win!`
+  } else if (cpuWins === playerWins) {
+    return resultOutput.textContent = `The results is: It's a tie!`
+  } else {
+    return resultOutput.textContent = `The results is: You lose!`
+  }
+}
+
+game();
+
+
+
+
+
+
